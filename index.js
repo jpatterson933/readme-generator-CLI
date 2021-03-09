bd2c465d266078570db8ea4fe125c3b8199d08a9
Jeffery Patterson <jpatterson933@ucla.edu>
2021-03-08 17:26:48 -0800

    finalized Readme Draft
    the readme draft has been finzlied, I will now copy and past it into our javascript, and then test this application to see if it populates a readme markdown file through the terminal

--- index.js
+++ index.js
@@ -3,33 +3,6 @@ console.log(process.argv)
 
 const fs = require('fs');
 
-
-//Directions and pseudocode
-
-
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-// inquirer
-
-//I am going to need a input for Title of Project
-//sections Description - Table of Contents - Installation - Usage - License - Contributing - Tests - Questions
-//User will need to enter a
-//Project Title
-//Description - Description 
-//Installation Instructions - Installation
-//Usage Information - Usage - instruct people how to use your project after it is installed
-//Contribution Guidelines - Contributing
-//Test Instructions - Test
-//--INFORMATION ENTERED WILL BE ADDED TO THE CORRESPONDING README SECTIONS
-//Next the user will choose a license for the application from a list of options
-//then a badge for that license is addd near the top of the readme and a notice is added to the section of the 
-//README entitled License that explains which license the application is covered under
-//User will enter GitHub username which will be added to -Questions- with a link to their profile
-//user will enter their email which will be added to the -Questions- with instruction of how to reach me with additional questions
-//when the user clicks on the links in the newly generated readme
-//then they are taken to the corresponding section of the readme
-//ROUGH DRAFT
 const inquirer = require('inquirer');
 inquirer
   .prompt([
@@ -52,6 +25,11 @@ inquirer
       type: 'input',
       message: 'Explain Application Usage: ',
       name: 'usage'
+    },
+    {
+      type: 'input',
+      message: 'What are your contribution guidelines?',
+      name: 'contribution',
     },
     {
       type: 'input',
--- readme.md
+++ readme.md
@@ -1,32 +1,40 @@
 # Title of Project
 
-### License: 
+## Notice licensetype licenseBadge
+
+This application is covered under insertlicense License
+License Detail: InsertLicenseDetails
 
 # Table of Contents
-<li> GitHub Details
-<li> Description
-<li> Installation
-<li> Usage
-<li> Testing
+1. [Description](#description)
+2. [Installation](#installation-instructions)
+3. [Usage](#application-usage)
+4. [Contribution Guidelines](#contribution-guidelines)
+5. [Testing](#testing-instrutions)
+6. [Questions](#questions)
 
-
-## GithubUsername
-Reach me directly at emailinsertedhere
-
-### [Live Application](url)
-### [GitHub Repository](url)
-
-# ProjectTitle Details 
-
-### Description
+# Description
 This is another line.
 
-### Installation Instructions
+# Installation Instructions
 Here is where the installation insturctions will go.
 
-### Application Usage
+# Application Usage
 What can this application be used for.
-### Testing Instrutions
+
+# Contribution Guidelines
+These are the guidelines the user will enter.
+
+# Testing Instrutions
 Here is how to test the application.
+
+# Questions
+If you have any questions that have not been answered, please send me an email: insertemailhere
+
+## Checkout My GitHub!
+
+### [GitHubUsername](urllinktoprofile)
+### [Live Application](url)
+### [GitHub Repository](url)
 
 
